e5ccfb712226841ccb0ba1e4e70ad43a2e8751a6
rakasan <racasan.vlad@gmail.com>
2022-11-06 23:29:55 +0200

    background + 2 spaceships visible

--- first_game/main.py
+++ first_game/main.py
@@ -1,2 +1,39 @@
 import pygame
+import os
 
+WIDTH, HEIGTH = 900,500
+WIN = pygame.display.set_mode((WIDTH,HEIGTH))
+WHITE = (255,255,255)
+FPS = 60
+SPACESHIP_WIDTH,SPACESHIP_HEIGHT = 55,40
+
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('first_game\Assets','spaceship_yellow.png'))
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE,(SPACESHIP_WIDTH,SPACESHIP_HEIGHT)),90)
+
+RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('first_game\Assets','spaceship_red.png'))
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE,(SPACESHIP_WIDTH,SPACESHIP_HEIGHT)),270)
+pygame.display.set_caption("First PyGame")
+
+def draw_window():
+    WIN.fill(WHITE)
+    WIN.blit(YELLOW_SPACESHIP,(300,100)) #draw a surface ontop the previous surface
+    WIN.blit(RED_SPACESHIP,(700,100)) #draw a surface ontop the previous surface
+    pygame.display.update() #update the display
+
+def main():
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get(): #get a list of all python events
+            if event.type == pygame.QUIT:
+                run = False
+
+        draw_window()
+
+    pygame.quit()
+
+
+
+if __name__ == "__main__":
+   main() #verification and check if this file is run directly and not throw an import
--- readme.txt
+++ readme.txt
@@ -1,1 +1,4 @@
 PyGame testing enviroment
+
+1st issue : pip has an issue with python 3.11, in which setup.py does not work. I had to downgrade to 3.10 in order to make it work.
+2nd issue : if you get a syntax error, with only the import pygame, you are tryng to run a Python program from inside a python shell. Change it to PowerShell, and close the python one (https://www.reddit.com/r/pygame/comments/sczt01/syntax_error_import_pygame/)
