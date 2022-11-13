ee77910dcca442c0a652e89500b570d523b31f40
rakasan <racasan.vlad@gmail.com>
2022-11-13 23:36:16 +0200

    added the bricks

--- second_game/main.py
+++ second_game/main.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 
 FPS = 60
@@ -11,6 +12,31 @@ WIDTH,HEIGHT = 800,600
 
 win = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Just a brick breaker")
+
+class Brick:
+    def __init__(self,x,y,width,height,health,color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.health = health
+        self.color = color
+
+    def draw(self,win):
+        pygame.draw.rect(win,self.color,(self.x,self.y,self.width,self.height))
+    
+    def collide(self,ball):
+        if not(ball.x < self.x + self.width and ball.x >= self.x):
+            return False
+        if not(ball.y - ball.radius <= self.y + self.height):
+            return False
+
+        self.hit()
+        ball.set_vel(ball.x_vel,ball.y_vel * -1)
+        return True
+    
+    def hit(self):
+        self.health -=1
 
 class Paddle:
     VEL = 5
@@ -49,19 +75,60 @@ class Ball:
     def draw(self, win):
         pygame.draw.circle(win,self.color,(self.x,self.y),self.radius)
 
-def draw(win,paddle,ball):
+def draw(win,paddle,ball,bricks):
     win.fill("white")
     ball.draw(win)
     paddle.draw(win)
+    for brick in bricks:
+        brick.draw(win)
+
     pygame.display.update()
 
+def ball_collisition(ball):
+    if ball.x -BALL_RADIUS <=0 or ball.x + BALL_RADIUS >= WIDTH:
+        ball.set_vel(ball.x_vel * -1,ball.y_vel)
+    if ball.y  + BALL_RADIUS >= HEIGHT or ball.y - BALL_RADIUS <= 0:
+        ball.set_vel(ball.x_vel,ball.y_vel * -1)
+
+def ball_paddle_collision(ball,paddle):
+    if not (ball.x < paddle.x + paddle.width and ball.x >= paddle.x):
+        return
+    if not (ball.y + ball.radius >= paddle.y) :
+        return
+
+    paddle_center = paddle.x + paddle.width/2
+    distance_to_center  = ball.x - paddle_center
+
+    percent_width = distance_to_center / paddle.width
+    angle = percent_width * 90
+    angle_radians = math.radians(angle)
+
+    x_vel = math.sin(angle_radians) * ball.VEL
+    y_vel = math.cos(angle_radians) * ball.VEL * -1
+
+    ball.set_vel(x_vel,y_vel)
+
+def generate_bricks(rows,colls):
+    gap_between_bricks = 2
+    brick_width =  WIDTH //colls - gap_between_bricks
+    brick_height = 20
+    bricks = []
+
+    for row in range(rows):
+        for col in range(colls):
+            brick = Brick(col  * brick_width + gap_between_bricks * col,
+                         row * brick_height  + gap_between_bricks * row,brick_width,brick_height,5,"green")
+            bricks.append(brick)
+
+    return bricks
 
 def main():
     clock = pygame.time.Clock()
     center_x = WIDTH/2  -PADDLE_WIDTH/2
     paddle_y = HEIGHT - PADDLE_HEIGHT - 5
     paddle = Paddle(center_x,paddle_y,PADDLE_WIDTH,PADDLE_HEIGHT,"black" )
-    ball = Ball(center_x - BALL_RADIUS,paddle_y- BALL_RADIUS,BALL_RADIUS,"black")
+    ball = Ball(WIDTH/2 - BALL_RADIUS,paddle_y- BALL_RADIUS,BALL_RADIUS,"black")
+    bricks = generate_bricks(3,10)
     run = True
     while run:
         clock.tick(FPS)
@@ -76,7 +143,17 @@ def main():
             paddle.move(1)
         
         ball.move()
-        draw(win,paddle,ball)
+        ball_collisition(ball)
+        ball_paddle_collision(ball,paddle)
+
+ 
+        for brick in bricks[:]:
+            brick.collide(ball)
+
+            if brick.health <=0:
+                bricks.remove(brick)
+
+        draw(win,paddle,ball,bricks)
 
     pygame.quit()
     quit()
